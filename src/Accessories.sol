3c1e4bc6b7ced7fb8d78a8d20f5d780d785ef6f0
coinop-logan <coinop.logan@gmail.com>
2023-08-23 18:07:37 -0800

    Basic skeleton setup

---
+++ src/Accessories.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+
+/* solhint-disable private-vars-leading-underscore */
+
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC721/ERC721.sol";
+import "openzeppelin/token/ERC721/IERC721.sol";
