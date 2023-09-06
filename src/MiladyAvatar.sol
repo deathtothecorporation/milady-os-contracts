90fd54707b7dadfa748f6e2a338ba94b46680d09
coinop-logan <coinop.logan@gmail.com>
2023-09-06 14:05:01 -0800

    remove Miladys as code dependency

--- src/MiladyAvatar.sol
+++ src/MiladyAvatar.sol
@@ -6,13 +6,13 @@ pragma solidity ^0.8.13;
 
 import "openzeppelin/token/ERC721/IERC721.sol";
 import "openzeppelin/utils/math/SafeMath.sol";
-import "./TokenBasedAccount.sol";
-import "../ERC6551/IERC6551Registry.sol";
-import "../ERC6551/IERC6551Account.sol";
-import "./Miladys.sol";
+import "./TBA/TokenBasedAccount.sol";
+import "./TBA/IERC6551Registry.sol";
+import "./TBA/IERC6551Account.sol";
+import "./Interfaces.sol";
 
-contract MiladyAvatar is IERC721 {
-    Miladys miladysContract;
+contract MiladyAvatar is IERC721, IMiladyAvatar {
+    IERC721 miladysContract;
     
     // state needed for TBA determination
     IERC6551Registry tbaRegistry;
@@ -21,36 +21,37 @@ contract MiladyAvatar is IERC721 {
 
     constructor(Miladys _miladysContract, IERC6551Registry _tbaRegistry, IERC6551Account _tbaAccountImpl, uint _chainId) {
         miladysContract = _miladysContract;
+        
         tbaRegistry = _tbaRegistry;
         tbaAccountImpl = _tbaAccountImpl;
         chainId = _chainId;
     }
 
-    function balanceOf(address owner) external view returns (uint256 balance) {
+    function balanceOf(address) external view returns (uint256 balance) {
         return 0;
     }
     function ownerOf(uint256 tokenId) external view returns (address owner) {
         return tbaRegistry.account(address(tbaAccountImpl), chainId, address(miladysContract), tokenId, 0);
     }
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external {
+    function safeTransferFrom(address, address, uint256, bytes calldata) external {
         revertWithSoulboundMessage();
     }
-    function safeTransferFrom(address from, address to, uint256 tokenId) external {
+    function safeTransferFrom(address, address, uint256) external {
         revertWithSoulboundMessage();
     }
-    function transferFrom(address from, address to, uint256 tokenId) external {
+    function transferFrom(address, address, uint256) external {
         revertWithSoulboundMessage();
     }
-    function approve(address to, uint256 tokenId) external {
+    function approve(address, uint256) external {
         revertWithSoulboundMessage();
     }
-    function setApprovalForAll(address operator, bool approved) external {
+    function setApprovalForAll(address, bool) external {
         revertWithSoulboundMessage();
     }
-    function getApproved(uint256 tokenId) external view returns (address operator) {
+    function getApproved(uint256) external view returns (address operator) {
         revert("Milady Dolls cannot be moved from their soulbound Milady.");
     }
-    function isApprovedForAll(address owner, address operator) external view returns (bool) {
+    function isApprovedForAll(address, address) external view returns (bool) {
         return false;
     }
     function revertWithSoulboundMessage() pure internal {
@@ -59,4 +60,6 @@ contract MiladyAvatar is IERC721 {
     function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         return interfaceId == type(IERC721).interfaceId;
     }
+
+    // todo: needs uri function
 }
