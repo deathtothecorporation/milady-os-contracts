4871ac6c10ed3eb6989e93590a5ea63a27e02b8b
coinop-logan <coinop.logan@gmail.com>
2023-09-24 13:42:58 -0800

    demonstration commit

--- test/TokenGatedAccount.t.sol
+++ test/TokenGatedAccount.t.sol
@@ -12,10 +12,6 @@ import "./MiladyOSTestBase.sol";
 import "./Miladys.sol";
 
 contract TGATests is MiladyOSTestBase {
-    // Schalk: I'm using an NFT "approve" tx for the inner tx to try to execute, for testing.
-    // Ideally this would instead be a straight send of ETH, to simplify the logic below
-    // But I have no idea how to structure this... tga.executeCall(targetAddress, ethValToSend, "") doesn't work.
-    
     function test_expectedPermissions() public {
         // send a Milady to a new address `firstNFTHolder`
         address firstNFTHolder = address(uint160(10));
@@ -23,39 +19,40 @@ contract TGATests is MiladyOSTestBase {
 
         // create TGA for NFT
         address payable tgaAddress = testUtils.createTGA(miladysContract, 0);
+        TokenGatedAccount tga = TokenGatedAccount(tgaAddress);
         
         // send that TGA some eth to play with
-        miladysContract.transferFrom(address(this), tgaAddress, 1);
-        // tgaAddress.transfer(100);
-
-        TokenGatedAccount tga = TokenGatedAccount(tgaAddress);
+        (bool sent, bytes memory data) = tgaAddress.call{value: 100}("");
+        require(sent, "Failed to send Ether");
 
         address someOtherAddress = address(uint160(11));
 
         vm.expectRevert("Unauthorized caller");
-        tga.executeCall(address(miladysContract), 0, abi.encodeCall(miladysContract.approve, (someOtherAddress, 1)));
+        tga.executeCall{value: 1}(address(this), 1, "");
 
-        vm.prank(firstNFTHolder);
-        tga.executeCall(address(miladysContract), 0, abi.encodeCall(miladysContract.approve, (someOtherAddress, 1)));
+        vm.startPrank(firstNFTHolder);
+        console.log(tga.owner(), address(this));
+        tga.executeCall{value: 1}(address(0x0), 1, "");
+        vm.stopPrank();
 
-        // test that bonded account can act
-        address bondedAccount = address(uint160(12));
-        vm.prank(firstNFTHolder);
-        tga.bond(bondedAccount);
-        vm.prank(bondedAccount);
-        tga.executeCall(address(miladysContract), 0, abi.encodeCall(miladysContract.approve, (someOtherAddress, 1)));
+        // // test that bonded account can act
+        // address bondedAccount = address(uint160(12));
+        // vm.prank(firstNFTHolder);
+        // tga.bond(bondedAccount);
+        // vm.prank(bondedAccount);
+        // tga.executeCall(address(miladysContract), 0, abi.encodeCall(miladysContract.approve, (someOtherAddress, 1)));
 
-        // test that this stops working once the base NFT is send somewhere else
-        vm.prank(firstNFTHolder);
-        miladysContract.transferFrom(firstNFTHolder, someOtherAddress, 0);
-        vm.prank(bondedAccount);
-        vm.expectRevert("Unauthorized caller");
-        tga.executeCall(address(miladysContract), 0, abi.encodeCall(miladysContract.approve, (someOtherAddress, 1)));
+        // // test that this stops working once the base NFT is send somewhere else
+        // vm.prank(firstNFTHolder);
+        // miladysContract.transferFrom(firstNFTHolder, someOtherAddress, 0);
+        // vm.prank(bondedAccount);
+        // vm.expectRevert("Unauthorized caller");
+        // tga.executeCall(address(miladysContract), 0, abi.encodeCall(miladysContract.approve, (someOtherAddress, 1)));
 
-        // now let's rebond the account and test that it can change the bonded account itself
-        vm.prank(someOtherAddress);
-        tga.bond(bondedAccount);
-        vm.prank(bondedAccount);
-        tga.bond(someOtherAddress);
+        // // now let's rebond the account and test that it can change the bonded account itself
+        // vm.prank(someOtherAddress);
+        // tga.bond(bondedAccount);
+        // vm.prank(bondedAccount);
+        // tga.bond(someOtherAddress);
     }
 }
