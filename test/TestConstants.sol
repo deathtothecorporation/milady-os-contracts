0f13b82ebf01bfec10e29566500bf3a3f67cd9cf
coinop-logan <coinop.logan@gmail.com>
2023-10-02 12:06:43 -0700

    remove comment

--- test/TestConstants.sol
+++ test/TestConstants.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.8.13;
 
 uint constant NUM_MILADYS_MINTED = 3; // 30 takes quite a while when forking from mainnet
 
-address constant MILADY_AUTHORITY_ADDRESS = address(uint160(1808100010810)); // mo entropy!
+address constant MILADY_AUTHORITY_ADDRESS = address(uint160(1808100010810));
 
-address payable constant PROJECT_REVENUE_RECIPIENT = payable(address(uint160(1808100010811))); // mo entropy!
+address payable constant PROJECT_REVENUE_RECIPIENT = payable(address(uint160(1808100010811)));
